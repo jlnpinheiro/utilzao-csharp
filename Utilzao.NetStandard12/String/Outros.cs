8d31e1be808550b513896233d45d8ae04d8856f9
Jorge Nogueira <jlnpinheiro@hotmail.com>
2018-05-12 00:37:42 -0300

    Commit inicial

---
+++ Utilzao.NetStandard12/String/Outros.cs
@@ -0,0 +1,78 @@
+﻿namespace JNogueira.Infraestrutura.Utilzao
+{
+    public static partial class ExtensionMethods
+    {
+        /// <summary>
+        /// Obtém o nome por extenso de um estado brasileiro a partir da sua sigla
+        /// </summary>
+        /// <param name="sigla">Sigla do estado brasileiro (por exemplo, ES, SP, RJ, etc).</param>
+        /// <returns>Nome por extenso do estado</returns>
+        public static string ObterNomeUfPorSiglaUf(this string sigla)
+        {
+            if (string.IsNullOrEmpty(sigla))
+                return string.Empty;
+
+            switch (sigla.Trim().ToUpper())
+            {
+                case "AC":
+                    return "Acre";
+                case "AL":
+                    return "Alagoas";
+                case "AP":
+                    return "Amapá";
+                case "AM":
+                    return "Amazonas";
+                case "BA":
+                    return "Bahia";
+                case "CE":
+                    return "Ceará";
+                case "DF":
+                    return "Distrito Federal";
+                case "ES":
+                    return "Espírito Santo";
+                case "GO":
+                    return "Goiás";
+                case "MA":
+                    return "Maranhão";
+                case "MT":
+                    return "Mato Grosso";
+                case "MS":
+                    return "Mato Grosso do Sul";
+                case "MG":
+                    return "Minas Gerais";
+                case "PA":
+                    return "Pará";
+                case "PB":
+                    return "Paraíba";
+                case "PR":
+                    return "Paraná";
+                case "PE":
+                    return "Pernambuco";
+                case "PI":
+                    return "Piauí";
+                case "RJ":
+                    return "Rio de Janeiro";
+                case "RN":
+                    return "Rio Grande do Norte";
+                case "RS":
+                    return "Rio Grande do Sul";
+                case "RO":
+                    return "Rondônia";
+                case "RR":
+                    return "Roraima";
+                case "SC":
+                    return "Santa Catarina";
+                case "SP":
+                    return "São Paulo";
+                case "SE":
+                    return "Sergipe";
+                case "TO":
+                    return "Tocantins";
+                case "EX":
+                    return "Exterior";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
---
+++ Utilzao.NetStandard12/String/Validacao.cs
@@ -0,0 +1,93 @@
+﻿namespace JNogueira.Infraestrutura.Utilzao
+{
+    public static partial class ExtensionMethods
+    {
+        /// <summary>
+        /// Valida se uma determinada string é o número válido de um CNPJ
+        /// </summary>
+        /// <param name="cnpj">Número do CNPJ que será validado.</param>
+        public static bool ValidarCnpj(this string cnpj)
+        {
+            if (string.IsNullOrEmpty(cnpj)) return false;
+
+            cnpj = cnpj.ExtrairNumeros().Trim();
+
+            if (cnpj.Length != 14)
+                return false;
+
+            var multiplicador1 = new[] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            var multiplicador2 = new[] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            var tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            resto = resto < 2 ? 0 : 11 - resto;
+            digito = resto.ToString();
+            tempCnpj += digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            resto = resto < 2 ? 0 : 11 - resto;
+            digito += resto;
+            return cnpj.EndsWith(digito);
+        }
+
+        /// <summary>
+        /// Valida se uma determinada string é o número válido de um CPF
+        /// </summary>
+        /// <param name="cpf">Número do CPF que será validado.</param>
+        public static bool ValidarCpf(this string cpf)
+        {
+            if (string.IsNullOrEmpty(cpf))
+                return false;
+
+            cpf = cpf.ExtrairNumeros().Trim();
+
+            if (cpf.Length != 11)
+                return false;
+
+            var multiplicador1 = new[] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            var multiplicador2 = new[] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            var tempCpf = cpf.Substring(0, 9);
+            var soma = 0;
+
+            for (var i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            var resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            var digito = resto.ToString();
+            tempCpf += digito;
+            soma = 0;
+            for (var i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito += resto;
+            return cpf.EndsWith(digito);
+        }
+
+        /// <summary>
+        /// Valida se uma determinada string é um número válido de CPF ou CNPJ
+        /// </summary>
+        /// <param name="input">Número do CPF ou CNPJ que será validado.</param>
+        public static bool ValidarCpfCnpj(this string input)
+        {
+            return !string.IsNullOrEmpty(input)
+                   && (input.RemoverCaracter(".", ",", "-", "/", "(", ")").Trim().Length == 11
+                       ? input.ValidarCpf()
+                       : input.ValidarCnpj());
+        }
+    }
+}
